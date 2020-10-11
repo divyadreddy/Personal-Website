6d65e98a18c10b181bc1e04b9897be9e43c3bec1
Glitch (hello-webpage) <none>
2020-10-11 10:25:15 +0000

    🐏🎤 Checkpoint ./projects.html:22137501/62 ./nav.html:22137501/368 ./script.js:22137501/2741 ./style/main.css:23649590/47

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
+  function change_nav_display() {
+    var x = document.getElementById("navbar");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
@@ -9,36 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
   $(function() {
-            $("#navbar").load("nav.html");
-          });
-  
-//   $.when($.ajax(enable_navbar())).then(function () {
-//     document.getElementById("about_me").click();
-//   });
-
-//   function enable_navbar(){
-//   var nav_options = ["about_me", "projects", "experience", "skills"];
-//   var opt;
-//   for (opt in nav_options) {
-//     console.log(opt, nav_options[opt]);
-//     console.log(document.getElementById(nav_options[opt]));
-//     if (document.getElementById(nav_options[opt])) {
-//       document
-//         .getElementById(nav_options[opt])
-//         .onclick = load_content(nav_options[opt]);
-//       // document.getElementById(nav_options[opt]).onclick = displayDate;
-//     }
-//   }
-//   }
-//     function load_content(content_id) {
-//       var content = document
-//         .getElementById("content");
-// // content.innerHTML = '';
-//           console.log(content_id + ".html");
-//           $(function() {
-//             $("#content").load(content_id + ".html");
-//           });
-//           //validation code to see State field is mandatory.
-//         }
-   
+    $("#navbar").load("nav.html");
+  });
+  $(".navbar").click(function() {
+    $(".nav li.active").removeClass("active");
+    $(this).addClass("active");
+  });
 });
