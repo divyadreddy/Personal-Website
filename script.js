7e507947104ea9373b348516cf46a68be6e9fe2a
Glitch (hello-webpage) <none>
2020-10-11 10:36:43 +0000

    😽🏖 Checkpoint ./index.html:23649590/3311 ./nav.html:23649590/206 ./script.js:23649590/621 ./skills.html:23649590/493 ./projects.html:23649590/1055 ./about_me.html:22137501/61

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-  function change_nav_display() {
-    var x = document.getElementById("navbar");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  $(function() {
-    $("#navbar").load("nav.html");
-  });
-  $(".navbar").click(function() {
-    $(".nav li.active").removeClass("active");
-    $(this).addClass("active");
-  });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   function change_nav_display() {
+//     var x = document.getElementById("navbar");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+//   $(function() {
+//     $("#navbar").load("nav.html");
+//   });
+//   $(".navbar").click(function() {
+//     $(".nav li.active").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
+
+function change_to_home() {
+  document.getElementById("content_1").src = "about_me.html";
+}
+function change_to_projects() {
+  document.getElementById("content_1").src = "projects.html";
+}
+function change_to_exp() {
+  document.getElementById("content_1").src = "experience.html";
+}
+function change_to_skills() {
+  document.getElementById("content_1").src = "skills.html";
+}
