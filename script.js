cb72c8584f5a703cf0c7d996cf24afcc45167e70
Glitch (hello-webpage) <none>
2020-10-11 08:42:19 +0000

    🌚😜 Checkpoint ./about_me.html:23649590/3176 ./index.html:22137501/1694 ./script.js:22137501/81

--- script.js
+++ script.js
@@ -10,33 +10,33 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   
   
-  $.when($.ajax(enable_navbar())).then(function () {
-    document.getElementById("about_me").click();
-  });
+//   $.when($.ajax(enable_navbar())).then(function () {
+//     document.getElementById("about_me").click();
+//   });
 
-  function enable_navbar(){
-  var nav_options = ["about_me", "projects", "experience", "skills"];
-  var opt;
-  for (opt in nav_options) {
-    console.log(opt, nav_options[opt]);
-    console.log(document.getElementById(nav_options[opt]));
-    if (document.getElementById(nav_options[opt])) {
-      document
-        .getElementById(nav_options[opt])
-        .onclick = load_content(nav_options[opt]);
-      // document.getElementById(nav_options[opt]).onclick = displayDate;
-    }
-  }
-  }
-    function load_content(content_id) {
-      var content = document
-        .getElementById("content");
-// content.innerHTML = '';
-          console.log(content_id + ".html");
-          $(function() {
-            $("#content").load(content_id + ".html");
-          });
-          //validation code to see State field is mandatory.
-        }
+//   function enable_navbar(){
+//   var nav_options = ["about_me", "projects", "experience", "skills"];
+//   var opt;
+//   for (opt in nav_options) {
+//     console.log(opt, nav_options[opt]);
+//     console.log(document.getElementById(nav_options[opt]));
+//     if (document.getElementById(nav_options[opt])) {
+//       document
+//         .getElementById(nav_options[opt])
+//         .onclick = load_content(nav_options[opt]);
+//       // document.getElementById(nav_options[opt]).onclick = displayDate;
+//     }
+//   }
+//   }
+//     function load_content(content_id) {
+//       var content = document
+//         .getElementById("content");
+// // content.innerHTML = '';
+//           console.log(content_id + ".html");
+//           $(function() {
+//             $("#content").load(content_id + ".html");
+//           });
+//           //validation code to see State field is mandatory.
+//         }
    
 });
